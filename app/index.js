16658c82a8ae5af0fb1e141be8a6dafdc6094f59
rulssss <marianopicco83@gmail.com>
2026-04-02 21:48:22 -0300

    se edito index.js para agregar POST Y GET

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
+const { PutCommand, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+const docClient = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ENV = process.env.NODE_ENV || "dev";
+const TABLE_NAME = "tasks";
 
-// middleware simple para logs estructurados
+app.use(express.json());
+
+// logs estructurados
 app.use((req, res, next) => {
   console.log(JSON.stringify({
     method: req.method,
@@ -14,20 +19,52 @@ app.use((req, res, next) => {
   next();
 });
 
-// healthcheck (clave en cloud)
-app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    env: ENV
-  });
+// healthcheck
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
-// endpoint base
-app.get('/', (req, res) => {
-  res.json({
-    message: "API running 🚀",
-    env: ENV
-  });
+// 🔥 crear tarea
+app.post("/tasks", async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const item = {
+      id: uuidv4(),
+      title,
+      completed: false
+    };
+
+    await docClient.send(
+      new PutCommand({
+        TableName: TABLE_NAME,
+        Item: item
+      })
+    );
+
+    res.status(201).json(item);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error creating task" });
+  }
+});
+
+// 🔥 listar tareas
+app.get("/tasks", async (req, res) => {
+  try {
+    const result = await docClient.send(
+      new ScanCommand({
+        TableName: TABLE_NAME
+      })
+    );
+
+    res.json(result.Items);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error fetching tasks" });
+  }
 });
 
 app.listen(PORT, () => {
